7780f469b69bcbdc4c76cd8e0e3e3a9674f1b343
amoyiki <amoyiki@gmail.com>
2016-12-13 16:13:43 +0800

    add new chrome extension demo

---
+++ QRCode/js/popup.js
@@ -0,0 +1,17 @@
+﻿onload=function(){
+  chrome.tabs.getSelected(function(tab){
+      //QRCode(元素id,相关配置文件)
+      var qrcode = new QRCode("qrcode-a", {
+              text: tab.url,
+              width: 160,
+              height: 160,
+              colorDark : '#000000',
+              colorLight : '#ffffff',
+              // QRCode的容错级别
+              correctLevel : QRCode.CorrectLevel.H
+            });
+    console.log(qrcode);
+  });
+}
+
+
